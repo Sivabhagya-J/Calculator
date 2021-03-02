573646e66fb94b90e3917bc5e8c63484878eec78
Sivabhagya J <sivabhagya.jawahar@ideas2it.com>
2021-03-02 22:16:07 +0530

    Changes in css | Script work - Committed

--- script/script.js
+++ script/script.js
@@ -1,30 +1,95 @@
+/**
+ * The method will calculate the arithmetic operations and display the output
+ * @author Sivabhagya Jawahar
+ * @param {number/operator} input 
+ */
 function calculate(input) {
     var displayElement = document.getElementById("calc-input");
     var value = displayElement.innerHTML;
     if (checkOperators(input)) {
         if (value) {
-            value = checkOperators(value.charAt(value.length-2)) ? value.replaceAt(value.length-2, input) : value + " " + input + " ";
+            value = checkOperators(value.charAt(value.length - 2)) ? value.replaceAt(value.length - 2, input) : value + " " + input + " ";
         }
+    } else if (input === ".") {
+        value = setDecimalInput(input, value).toString();
     } else if (input !== "=") {
-        value += input;
+        value = checkValidInput(input, value).toString();
     }
-    var expression = value.replace("x", "*");
     displayElement.innerHTML = value;
-    var result = eval(expression);
-    //result = (result % 1 === 0) ? result : result.toFixed(3);
-    document.getElementById("calc-result").innerHTML = result ? result : "";
+    var expression = value.replaceAll("x", "*");
+    expression = expression.replaceAll(" ", "");
+    if (expression && expression.length && !checkOperators(expression[expression.length - 1])) {
+        var result = eval(expression);
+        document.getElementById("calc-result").innerHTML = result != undefined ? Math.round(result * Math.pow(10, 3)) / Math.pow(10, 3) : "";
+    }
 }
 
+/**
+ * The method will clear the display value and result
+ * @author Sivabhagya Jawahar
+ */
 function clearResult() {
     document.getElementById("calc-input").innerHTML = "";
     document.getElementById("calc-result").innerHTML = "";
 }
 
+/**
+ * The method will check whether the input value is operators are not
+ * @author Sivabhagya Jawahar
+ * @param {number/operator} input 
+ * @returns true/false
+ */
 function checkOperators(input) {
     return (input === "+" || input === "-" || input === "x" || input === "/" || input === "%");
 }
 
-String.prototype.replaceAt = function(index, replacement) {
+/**
+ * The method is used for replaceAt operation
+ * @author Sivabhagya Jawahar
+ * @param {*} index 
+ * @param {*} replacement 
+ * @returns replaced value
+ */
+String.prototype.replaceAt = function (index, replacement) {
     return this.substr(0, index) + replacement + this.substr(index + replacement.length);
 }
 
+/**
+ * The method will check for the valid input for calculation
+ * @author Sivabhagya Jawahar
+ * @param {number/operator} input 
+ * @param {number/operator} value 
+ * @returns input/value based on the user's valid input
+ */
+function checkValidInput(input, value) {
+    if (value) {
+        var lastSpace = value.lastIndexOf(" ");
+        if (lastSpace >= 0) {
+            var substr = value.substr(lastSpace + 1, value.length);
+            value = substr ? value.slice(0, lastSpace + 1) : value;
+            value += Number(substr + input);
+            return value;
+        }
+        return Number(value + input);
+    }
+    return input;
+}
+
+/**
+ * The method is used to set the decimal point for calculation
+ * @author Sivabhagya Jawahar
+ * @param {number} input 
+ * @param {number/operator} value 
+ * @returns value/input based on the user's valid input
+ */
+function setDecimalInput(input, value) {
+    if (value) {
+        var lastSpace = value.lastIndexOf(" ");
+        if (lastSpace >= 0) {
+            var substr = value.substr(lastSpace + 1, value.length);
+            return isNaN(substr + input) ? value : value + input;
+        }
+        return isNaN(value + input) ? value : value + input;
+    }
+    return "0.";
+}
